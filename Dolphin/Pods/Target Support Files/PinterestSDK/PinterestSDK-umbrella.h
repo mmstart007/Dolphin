3d29262d0ba70112270b7f79dfe865257aba76af
Derek Stock <derek@ninthcoast.com>
2016-12-09 03:25:23 +0800

    Swift 3.0 second commit

---
+++ Dolphin/Pods/Target Support Files/PinterestSDK/PinterestSDK-umbrella.h
@@ -0,0 +1,18 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "PDKBoard.h"
+#import "PDKCategories.h"
+#import "PDKClient.h"
+#import "PDKImageInfo.h"
+#import "PDKInterest.h"
+#import "PDKModelObject.h"
+#import "PDKPin.h"
+#import "PDKResponseObject.h"
+#import "PDKUser.h"
+#import "PinterestSDK.h"
+
+FOUNDATION_EXPORT double PinterestSDKVersionNumber;
+FOUNDATION_EXPORT const unsigned char PinterestSDKVersionString[];
+
